df5a1f4ae815405b37faaefc0431647084683663
greymtr <greymtr@disroot.org>
2022-06-16 06:00:30 +0530

    2022-06-16.06:00 -

---
+++ server/refresh-config.sh
@@ -0,0 +1,11 @@
+wg-quick down wg-seraph
+
+cp head-config /etc/wireguard/wg-seraph.conf
+
+for i in $(ls peers)
+do
+	cat peers/$i >> /etc/wireguard/wg-seraph.conf
+	echo "" >> /etc/wireguard/wg-seraph.conf
+done
+
+wg-quick up wg-seraph
